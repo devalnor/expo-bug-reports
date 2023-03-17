0e48a00ea32bf35891c82170483c395213da437d
Maxime de Visscher <maxime.de.visscher@gmail.com>
2023-03-17 15:35:31 +0100

    🐛 Reproducible bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Updates from 'expo-updates'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This is app use channel "{Updates.channel || 'Unknow'}" !</Text>
       <StatusBar style="auto" />
     </View>
   );
